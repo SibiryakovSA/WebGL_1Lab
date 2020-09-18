1f25b60963e22b0711b091d64ae616c9b60cc1ef
SibiryakovSA <19999semen@mail.ru>
2020-09-18 21:19:06 +0500

    мелкие фиксы1

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -9,6 +9,6 @@ void main(){
 	g = 0.0;
 	//b = sin(position[1] * 180.0);												//горизонтальные линии
 	//b = tan((sin(position[0] * 30.0) + position[1] * 20.0) * 5.0);			//синусоиды
-	b = sin((position[0] * position[0] + position[1] * position[1]) * 300.0);	//элипсоиды
+	b = sin((position[0] * position[0] + position[1] * position[1]) * 300.0);	//эллипсы
 	gl_FragColor = vec4(r, g, b, 1.0);	//цвет, соответственно R, G, B, A
 }
